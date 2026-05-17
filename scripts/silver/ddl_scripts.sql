a5d044ccc0cc57a0d7503c2a3979ad49c7eb0402
goldendevelopers999-max <goldendevelopers999@gmail.com>
2026-05-17 17:54:20 +0530

    Update ddl_scripts.sql

--- scripts/silver/ddl_scripts.sql
+++ scripts/silver/ddl_scripts.sql
@@ -8,7 +8,8 @@ create table silver.crm_cust_info (
 	cst_lastname nvarchar(50),
 	cst_marital_status nvarchar(10),
 	cst_gndr nvarchar(10),
-	cst_create_date date
+	cst_create_date date,
+    dwh_create_date datetime2 default getdate()
 );
 
 if object_id (silver.crm_prd_info, 'U') is not null
@@ -20,7 +21,8 @@ create table silver.crm_prd_info (
 	prd_cost int,
 	prd_line nvarchar(50),
 	prd_start_dt datetime,
-	prd_end_dt datetime
+	prd_end_dt datetime,
+    dwh_create_date datetime2 default getdate()
 );
 
 
@@ -35,7 +37,8 @@ create table silver.crm_sales_details (
 	sls_due_dt int,
 	sls_sales int,
 	sls_quantity int,
-	sls_price int
+	sls_price int,
+    dwh_create_date datetime2 default getdate()
 );
 
 
@@ -44,7 +47,8 @@ if object_id (silver.erp_cust_az12, 'U') is not null
 create table silver.erp_cust_az12 (
 	cid nvarchar(50),
 	bdate date,
-	gen nvarchar(50)
+	gen nvarchar(50),
+    dwh_create_date datetime2 default getdate()
 );
 
 
@@ -52,7 +56,8 @@ if object_id (silver.erp_loc_a101, 'U') is not null
  drop table silver.crm_cust_info;
 create table silver.erp_loc_a101 (
 	cid nvarchar(50),
-	cntry nvarchar(50)
+	cntry nvarchar(50),
+    dwh_create_date datetime2 default getdate()
 );
 
 
@@ -62,5 +67,6 @@ create table silver.erp_px_cat_g1v2 (
 	id nvarchar(50),
 	cat nvarchar(50),
 	subcat nvarchar(50),
-	maintenance nvarchar(50)
+	maintenance nvarchar(50),
+    dwh_create_date datetime2 default getdate()
 );
